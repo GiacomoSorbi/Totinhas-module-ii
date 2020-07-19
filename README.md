11fc653c4e1b0a36900e442f84a0e3023f9254df
Sara Bretes <sarinha@saras-mbp.mynet>
2020-07-19 15:29:27 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Totinhas-module-ii
-
-`gh-pages` enabled
---
+++ precache-manifest.3b1a010722155cbcc181d45c4752a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef6434d8e77bfc3a752100309304bcc",
+    "url": "/Totinhas-module-ii/index.html"
+  },
+  {
+    "revision": "a9f91cdcb4be3bda8a5a",
+    "url": "/Totinhas-module-ii/static/css/main.69e60527.chunk.css"
+  },
+  {
+    "revision": "0cf72e7c3d0bc1d1bf9f",
+    "url": "/Totinhas-module-ii/static/js/2.ccb260fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Totinhas-module-ii/static/js/2.ccb260fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f91cdcb4be3bda8a5a",
+    "url": "/Totinhas-module-ii/static/js/main.d8fd58e7.chunk.js"
+  },
+  {
+    "revision": "efa67f5a7af24a27b4d8",
+    "url": "/Totinhas-module-ii/static/js/runtime-main.767ad187.js"
+  }
+]);
