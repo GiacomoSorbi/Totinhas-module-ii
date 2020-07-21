e7840d1f7d8b25359554c26c66e8f1771dbf2b5c
Sara Bretes <sarinha@saras-mbp.mynet>
2020-07-21 23:21:35 +0100

    updated links for social media

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,10 +6,18 @@ const Footer = () => {
     <div className="Footer">
       <div className="copyRight">2020 - All rights reserved &copy;</div>
       <div className="socialMedia">
-        <i className="fab fa-facebook-f"></i>
-        <i className="fab fa-linkedin-in"></i>
-        <i className="fab fa-twitter"></i>
-        <i className="fas fa-envelope"></i>
+        <a href="http://www.facebook.com" target="_blank">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+        <a href="http://www.linkedin.com">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="http://www.twitter.com" target="_blank">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="mailto:sluisc01@mail.bbk.ac.uk?subject=your react project">
+          <i className="fas fa-envelope"></i>
+        </a>
       </div>
     </div>
   );
